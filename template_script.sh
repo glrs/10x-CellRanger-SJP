e89abdcb9438be38a830052df0b5efedfd9648ac
glrs <glaros.t@gmail.com>
2017-02-02 17:08:56 +0100

    Functionality for adapting to the various calls The script could be called from a symlink or from the original file. However the workspace should always be that of the original file.

--- template_script.sh
+++ template_script.sh
@@ -18,10 +18,29 @@
 ?job_description
 #SBATCH -J
 
-# Always change directory to the current folder
-# (which should always be the project folder)
-# This helps the orientation of the script.
-cd "$(dirname "$0")"
+#  -- Always change directory to the project's --
+# root dir where the script is located. This helps the
+# orientation of the script relative to the project.
+
+# Get which file was called on the bash command
+#    symlink --or-- original
+SOURCE="${BASH_SOURCE[0]}"
+
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ];
+do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+
+  # if $SOURCE was a relative symlink, we need to resolve it
+  # relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+
+# Form the directory and move there
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $DIR
+
 
 # Define the project variables given as arguments
 ?reference_genome
